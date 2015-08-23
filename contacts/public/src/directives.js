f9b1b2f3a43561a15b4c0279d0770173ecf28555
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2015-08-23 17:23:10 -0300

    Finishing directive and fix issue 'ng-messages no longer interprets its attribute value properly #11616'

--- contacts/public/src/directives.js
+++ contacts/public/src/directives.js
@@ -11,4 +11,44 @@ angular.module('ContactsApp')
         url: ['URL', 'should be an URL'],
         tel: ['Phone Number', 'should be phone number'],
         color: ['Color', 'should be color']
+    })
+    .directive('formField', function ($timeout, FieldTypes) {
+        return {
+            restrict: 'EA' ,
+            templateUrl: 'views/form-field.html',
+            replace: true,
+            scope: {
+                record: '=',
+                field: '@',
+                live: '@',
+                required: '@'
+            },
+            link: function ($scope, element, attr) {
+                
+                $scope.$on('record:invalid', function () {
+                    $scope[$scope.field].$setDirty();
+                });
+                
+                $scope.types = FieldTypes;
+                
+                $scope.remove = function (field) {
+                    delete $scope.record[field];
+                    $scope.blurUpdate();
+                };
+                
+                $scope.blurUpdate = function () {
+                    if ($scope.live !== 'false') {
+                        $scope.record.$update(function (updateRecord) {
+                            $scope.record = updateRecord;
+                        });
+                    }
+                };
+                
+                var saveTimeout;
+                $scope.update = function () {
+                    $timeout.cancel(saveTimeout);
+                    saveTimeout = $timeout($scope.blurUpdate, 1000);
+                };
+            }
+        };
     });
